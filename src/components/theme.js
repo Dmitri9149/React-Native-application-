d1ed4887d0419f30e650b25d7dd648d60e7d1444
Dmitri Dmitri <dmitri@example.com>
2024-08-18 19:30:26 +0300

    add/refactor colors

---
+++ src/components/theme.js
@@ -0,0 +1,26 @@
+const theme = {
+  bgColors: {
+    defaultDark: "black",
+    default: "white",
+    textPrimary: '#24292e',
+    textSecondary: '#586069',
+    primary: '#0366d6',
+  },
+  colors: {
+    default: "black",
+    darkBackground: "white",
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+  },
+  fonts: {
+    main: 'System',
+  },
+  fontWeights: {
+    normal: '400',
+    bold: '700',
+  },
+};
+
+export default theme;

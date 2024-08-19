58e8a839f974465391014f5ae98f36cd902c7d89
Dmitri Dmitri <dmitri@example.com>
2024-08-19 13:46:53 +0300

    use customized 'Text' module

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,6 +1,7 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
+import Text from './Text'
 
 const styles = StyleSheet.create({
   container: {

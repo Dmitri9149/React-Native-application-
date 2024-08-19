2d74c7b9bfdca695aaf6ab6395041e19b5d50b3e
Dmitri Dmitri <dmitri@example.com>
2024-08-19 13:32:41 +0300

    refactor the theme code

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,15 +1,15 @@
 import { View, StyleSheet, Text } from 'react-native';
 import Constants from 'expo-constants';
-import theme from './theme';
+import theme from '../theme1';
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: theme.bgColors.textPrimary,
+    backgroundColor: theme1.bgColors.textPrimary,
   },
   pressable: {
-    color: theme.colors.darkBackground,
-    fontWeight: "bold",
+    color: theme1.colors.darkBackground,
+    fontWeight: theme1.fontWeights.bold,
     padding: 15,
 }
 });

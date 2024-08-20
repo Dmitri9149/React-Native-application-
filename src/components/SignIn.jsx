0a5c1d604c258c16cd55bac607ebfb4d5f8f9e4b
Dmitri Dmitri <dmitri@example.com>
2024-08-20 20:01:02 +0300

    simple SighInForm, initial commit, just console.log inputed 'username' and 'password'

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,43 @@
+import { TextInput, Pressable, View } from 'react-native';
+import { Formik, useFormik  } from 'formik';
+
 import Text from './Text';
+
+const initialValues = {
+    username: '',
+    password: '',
+  };
+
+const SignInForm = ({onSubmit}) => {
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+      });
+
+      return (
+        <View>
+          <TextInput
+            placeholder="Username"
+            value={formik.values.username}
+            onChangeText={formik.handleChange('username')}
+          />
+          <TextInput
+            placeholder="Password"
+            value={formik.values.password}
+            onChangeText={formik.handleChange('password')}
+          />
+          <Pressable onPress={formik.handleSubmit}>
+            <Text>Submit</Text>
+          </Pressable>
+        </View>
+      );
+};
+
 
 const SignIn = () => {
     const onSubmit = (values)  => {
-        username = values.username;
-        password = values.password;
+    const { username, password}  = values;
+    console.log(`Password: ${password} and Username: ${username}`)
     } 
   return <SignInForm onSubmit={onSubmit}/>;
 };
